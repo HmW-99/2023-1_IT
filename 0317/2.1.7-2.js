f6e8d0d476128e2321aeb1c912db4e7c38ee305d
HmW-99 <81504072+HmW-99@users.noreply.github.com>
2023-03-17 21:59:33 +0900

    t4

---
+++ 0317/2.1.7-2.js
@@ -0,0 +1,27 @@
+const condition = true; // true면 resolve, false면 reject
+const promise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('성공');
+  } else {
+    reject('실패');
+  }
+});
+promise
+  .then((message) => {
+    return new Promise((resolve, reject) => {
+      resolve(message);
+    });
+  })
+  .then((message2) => {
+    console.log(message2);
+    return new Promise((resolve, reject) => {
+      resolve(message2);
+    });
+  })
+  .then((message3) => {
+    console.log(message3);
+  })
+
+  .catch((error) => {
+    console.error(error);
+  });
